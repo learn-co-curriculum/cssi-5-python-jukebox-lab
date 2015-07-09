b53798c00b5ce7ea0daa34709ad1af173f3ab53a
georgiadavis <gyang925@gmail.com>
2015-07-08 23:59:23 -0700

    first commit

---
+++ jukebox.py
@@ -0,0 +1,17 @@
+def say_hello(name):
+  return "Hi {}".format(name)
+
+users_name= raw_input("Enter your name: ")
+
+print(say_hello(users_name))
+
+
+print songs
+
+
+
+def import_songs():
+    
+songs = ["uptown funk", "thinking out loud" , "blank space" , "take me to church" , "shake it off"]
+for song in songs:
+   print song
